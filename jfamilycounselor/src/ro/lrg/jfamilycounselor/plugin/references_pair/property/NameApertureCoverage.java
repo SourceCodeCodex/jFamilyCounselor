c907ffecfff5600846b1cd478f1e9f0fc4799dd6
Petru Florin Mihancea <petrum@gmail.com>
2023-06-13 20:37:56 +0300

    fix metrics for explanation and fix tests

--- jfamilycounselor/src/ro/lrg/jfamilycounselor/plugin/references_pair/property/NameApertureCoverage.java
+++ jfamilycounselor/src/ro/lrg/jfamilycounselor/plugin/references_pair/property/NameApertureCoverage.java
@@ -1,15 +1,23 @@
 package ro.lrg.jfamilycounselor.plugin.references_pair.property;
 
 import jfamilycounselor.metamodel.entity.MReferencesPair;
+import jfamilycounselor.metamodel.entity.MTypesPair;
+import ro.lrg.xcore.metametamodel.Group;
 import ro.lrg.xcore.metametamodel.IPropertyComputer;
 import ro.lrg.xcore.metametamodel.PropertyComputer;
 
 @PropertyComputer
-public class NameApertureCoverage implements IPropertyComputer<Double, MReferencesPair> {
+public class NameApertureCoverage 
+	implements IPropertyComputer<Double, MReferencesPair> {
 
-    @Override
     public Double compute(MReferencesPair mReferencesPair) {
-	return (1.0 * mReferencesPair.nameUsedTypes().getElements().size()) / mReferencesPair.aperture();
+    	Group<MTypesPair> usedConcreteTypePairs 
+    		= mReferencesPair.nameUsedTypes();
+    	double cardinalityUsedConcreteTypePairs 
+    		= usedConcreteTypePairs.getElements().size();
+    	double cardinalityPossibleConcreteTypePairs 
+    		= mReferencesPair.aperture();
+    	return cardinalityUsedConcreteTypePairs 
+    			/ cardinalityPossibleConcreteTypePairs;
     }
-
 }
