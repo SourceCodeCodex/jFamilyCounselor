e10ea63962e66651344cd2d5c0768370c3a2890b
Alin-Petru Roșu <rosualinpetru@gmail.com>
2023-06-20 11:23:54 +0300

    Update the WineBar example project

--- jfamilycounselor.example/WineBar/src/ro/lrg/winebar/WaiterTray.java
+++ jfamilycounselor.example/WineBar/src/ro/lrg/winebar/WaiterTray.java
@@ -1,20 +1,14 @@
 package ro.lrg.winebar;
-
-public class WaiterTray {
+class WaiterTray {
 	private Wine _wine;
-	private Glass _glass;
-	
-	public void setBoth(Wine wineB, Glass glassB) {
-		_wine = wineB;
-		_glass = glassB;
-	}
-	
+	private WineGlass _glass;
 	public void setWine(Wine wineP) {
 		_wine = wineP;
 	}
-	
-	public void setGlass(Glass glassP) {
+	public void setGlass(WineGlass glassP) {
 		_glass = glassP;
 	}
-		
+	public void serve() {
+		_wine.pourInto(_glass);
+	}
 }
--- jfamilycounselor.example/WineBar/src/ro/lrg/winebar/Wine.java
+++
@@ -1,7 +0,0 @@
-package ro.lrg.winebar;
-
-interface Wine { }
-
-class RedWine implements Wine { }
-
-class WhiteWine implements Wine { }
