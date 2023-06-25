453d9d1cf14421a741fc4c41b433cb845077e27c
BOGDAN <bogdan.pirtica@student.upt.ro>
2023-06-25 18:56:02 +0300

    move code to new branch, include all leaves in the diagram, remove library classes

---
+++ jfamilycounselor/src/ro/lrg/jfamilycounselor/diagram/ChordDiagram.java
@@ -0,0 +1,53 @@
+package ro.lrg.jfamilycounselor.diagram;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import ro.lrg.jfamilycounselor.util.logging.jFCLogger;
+
+public class ChordDiagram {
+	private static final Logger logger = jFCLogger.getLogger();
+	private final URL diagramHtmlUrl;
+	
+	public ChordDiagram(File htmlFilePath) throws MalformedURLException {
+		diagramHtmlUrl = Paths.get(htmlFilePath.getAbsolutePath()).toUri().toURL();
+	}
+
+	public Optional<java.nio.file.Path> getDiagramHtmlPath() {
+		var bundle = Platform.getBundle("jfamilycounselor");
+		var sourceFolderUrl = FileLocator.find(bundle, new Path("src"), null);
+
+		try {
+			var sourceFolderPath = FileLocator.toFileURL(sourceFolderUrl).getPath();
+			var htmlPath = Paths.get(sourceFolderPath, "ro", "lrg", "jfamilycounselor", "diagram", "chord-diagram.html");
+			return Optional.of(htmlPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Optional.empty();
+		}
+	}
+
+	public void startBrowser() {
+		try {
+			var support = PlatformUI.getWorkbench().getBrowserSupport();
+			var browser = support.createBrowser(IWorkbenchBrowserSupport.AS_VIEW, null, null, null);
+			
+			var diagramUrl = diagramHtmlUrl;
+			browser.openURL(diagramUrl);
+		} catch (PartInitException e) {
+			logger.warning("PartInitException encountered: " + e.getMessage());
+		}
+	}
+}
---
+++ jfamilycounselor/src/ro/lrg/jfamilycounselor/diagram/ExportDiagramRelationsJob.java
@@ -0,0 +1,273 @@
+package ro.lrg.jfamilycounselor.diagram;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayDeque;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeHierarchy;
+import org.eclipse.jdt.core.JavaModelException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jfamilycounselor.metamodel.factory.Factory;
+import ro.lrg.jfamilycounselor.report.RelevantTypesJob;
+import ro.lrg.jfamilycounselor.util.Constants;
+import ro.lrg.jfamilycounselor.util.Constants.EstimationType;
+import ro.lrg.jfamilycounselor.util.datatype.Pair;
+import ro.lrg.jfamilycounselor.util.logging.jFCLogger;
+
+public class ExportDiagramRelationsJob extends Job {
+
+	public static final String FAMILY = "jFamilyCounselorExportReport";
+	private static final double NAME_BASED_THRESHOLD = 0.5;
+
+	public static final ISchedulingRule MUTEX = new ISchedulingRule() {
+		public boolean contains(ISchedulingRule rule) {
+			return rule == this;
+		}
+
+		public boolean isConflicting(ISchedulingRule rule) {
+			return rule == this;
+		}
+	};
+
+	private static Logger logger = jFCLogger.getLogger();
+
+	private final IJavaProject iJavaProject;
+	private final EstimationType estimation;
+
+	public ExportDiagramRelationsJob(EstimationType estimation, IJavaProject iJavaProject) {
+		super(estimation + " exporting report...");
+		this.iJavaProject = iJavaProject;
+		this.estimation = estimation;
+	}
+
+	private void writeStringToFile(File outputFile, String outputString) throws IOException {
+		outputFile.createNewFile();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile.getAbsolutePath()));
+		writer.write(outputString);
+		writer.flush();
+		writer.close();
+	}
+
+	private IPath createExportFolder() throws JavaModelException {
+		var wsRoot = iJavaProject.getProject().getWorkspace().getRoot();
+
+		IPath outputFolder;
+
+		outputFolder = wsRoot.getFolder(iJavaProject.getOutputLocation()).getLocation();
+
+		var formatter = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss");
+		var timestamp = formatter.format(new Date());
+
+		var reportsFolder = outputFolder.append("jFamilyCounselor-diagrams");
+		reportsFolder = reportsFolder.append(String.format("%s-%s-diagram-%s", iJavaProject.getElementName(),
+				estimation.toString(), timestamp.toString()));
+		reportsFolder.toFile().mkdirs();
+
+		return reportsFolder;
+	}
+
+	protected IStatus run(IProgressMonitor monitor) {
+		var relevantTypesJob = new RelevantTypesJob(iJavaProject, EstimationType.NAME_BASED);
+		relevantTypesJob.setPriority(Job.LONG);
+		relevantTypesJob.setSystem(false);
+		relevantTypesJob.setUser(true);
+		relevantTypesJob.schedule();
+		try {
+			relevantTypesJob.join();
+		} catch (InterruptedException e) {
+			logger.warning("Thread interrupted: ExportReportJob");
+			return Status.CANCEL_STATUS;
+		}
+
+		var relevantTypes = relevantTypesJob.relevantTypes();
+
+		var start = Instant.now();
+
+		List<Pair<IType, IType>> typePairs = relevantTypes.parallelStream()
+				.flatMap(t -> Factory.getInstance().createMType(t).relevantReferencesPairs().getElements().stream()
+						.filter(r -> r.nameBasedApertureCoverage() <= NAME_BASED_THRESHOLD)
+						.flatMap(r -> r.nameBasedUsedTypes().getElements().stream().map(p -> {
+							Pair<?, ?> pair = p.getUnderlyingObject();
+							IType p1IType = ((IType) pair._1);
+							IType p2IType = ((IType) pair._2);
+
+							if (p1IType.toString().compareTo(p2IType.toString()) > 0)
+								return new Pair<>(p2IType, p1IType);
+
+							return new Pair<>(p1IType, p2IType);
+						})).distinct())
+				.collect(Collectors.toList());
+
+		Map<String, ITypeHierarchy> typeHierarchiesMap = new ConcurrentHashMap<>();
+		Map<String, String> leavesMap = new ConcurrentHashMap<>();
+		
+		typePairs.stream().flatMap(p -> Stream.of(p._1, p._2)).distinct().parallel().forEach(t -> {
+			try {
+				ITypeHierarchy typeHierarchy = t.newTypeHierarchy(null);
+
+				// check that the type is at the base of the hierarchy
+				if (typeHierarchy.getSubclasses(t).length != 0) {
+					return;
+				}
+				
+				leavesMap.putIfAbsent(t.getFullyQualifiedName(), t.getFullyQualifiedName());
+				
+				var superClasses = typeHierarchy.getAllSuperclasses(t);
+				// contains only Object
+				// TODO: can length be 0 ?
+				if (superClasses.length == 1) {
+					typeHierarchiesMap.putIfAbsent(t.getFullyQualifiedName(), typeHierarchy);
+					return;
+				}
+				
+				var rootClass = superClasses[superClasses.length - 2];
+				
+				// if the root class is from a java library take the highest class from the project
+				if(rootClass.isBinary()) {
+					var  implementedClasses = Stream.of(superClasses).takeWhile(c -> !c.isBinary()).collect(Collectors.toList());
+					if(implementedClasses.size() == 0) {
+						return;
+					}
+					rootClass = implementedClasses.get(implementedClasses.size() - 1);
+				}
+				
+				var rootClassFQN = rootClass.getFullyQualifiedName();
+				if (!typeHierarchiesMap.containsKey(rootClassFQN)) {
+					typeHierarchiesMap.put(rootClassFQN, rootClass.newTypeHierarchy(null));
+				}
+			} catch (JavaModelException e) {
+				return;
+			}
+		});
+
+		var hierarchiesList = new ConcurrentLinkedQueue<ParentLink>();
+		hierarchiesList.add(new ParentLink("", Constants.OBJECT_FQN));
+		typeHierarchiesMap.entrySet().parallelStream().forEach(h -> {
+			var rootClassHierarchy = h.getValue();
+			
+			// pair of parent and child IType
+			var q = new ArrayDeque<Pair<String, IType>>();
+			q.add(new Pair<String, IType>(Constants.OBJECT_FQN, rootClassHierarchy.getType()));
+			
+			while (q.size() != 0) {
+				var pair = q.remove();
+				var newParent = pair._2.getFullyQualifiedName();
+				hierarchiesList.add(new ParentLink(pair._1, newParent));
+
+				q.addAll(Stream.of(rootClassHierarchy.getSubclasses(pair._2)).map(c -> new Pair<>(newParent, c))
+						.collect(Collectors.toList()));
+			}
+		});
+
+		typeHierarchiesMap.values().stream().forEach(h -> {
+			var superClasses = h.getAllSuperclasses(h.getType());
+			if (superClasses.length == 1) {
+				// TODO: something else
+				return;
+			}
+			var rootClass = superClasses[superClasses.length - 2];
+
+			ITypeHierarchy rootHierachy = null;
+			try {
+				rootHierachy = rootClass.newTypeHierarchy(null);
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			var q = new ArrayDeque<IType>();
+			q.add(rootClass);
+			while (q.size() != 0) {
+				var hierarchy = q.remove();
+				System.out.println(hierarchy.getFullyQualifiedName());
+				q.addAll(List.of(rootHierachy.getSubclasses(hierarchy)));
+			}
+		});
+
+		List<List<String>> pairsFQNs = typePairs.parallelStream().flatMap(p -> {
+			var p1FQN = p._1.getFullyQualifiedName();
+			var p2FQN = p._2.getFullyQualifiedName();
+			if (leavesMap.containsKey(p1FQN) && leavesMap.containsKey(p2FQN)) {
+				return Stream.of(List.of(p1FQN, p2FQN));
+			}
+			return Stream.empty();
+		}).collect(Collectors.toList());
+
+		var objectMapper = new ObjectMapper().writerWithDefaultPrettyPrinter();
+
+		IPath reportsFolder;
+		try {
+			reportsFolder = createExportFolder();
+			var diagramOutputFile = reportsFolder.append("chord-diagram.html").toFile();
+			var pairsJsonFile = reportsFolder.append("pairs.js").toFile();
+			var hierarchiesJsonFile = reportsFolder.append("hierarchies.js").toFile();
+
+			var pairsJson = objectMapper.writeValueAsString(pairsFQNs);
+			var leavesDataOutput = String.format("var pairs = %s;", pairsJson);
+
+			var hierarchiesJson = objectMapper.writeValueAsString(hierarchiesList.stream().collect(Collectors.toSet()));
+			var hierarchiesDataOutput = String.format("var hierarchies = %s;", hierarchiesJson);
+
+			writeStringToFile(pairsJsonFile, leavesDataOutput);
+			writeStringToFile(hierarchiesJsonFile, hierarchiesDataOutput);
+
+			try {
+				var chordDiagram = new ChordDiagram(diagramOutputFile);
+				var diagramHtmlPath = chordDiagram.getDiagramHtmlPath();
+				if (diagramHtmlPath.isEmpty())
+					return Status.error("The diagram html source could not be found");
+				writeStringToFile(diagramOutputFile, Files.readString(diagramHtmlPath.get()));
+
+				chordDiagram.startBrowser();
+			} catch (MalformedURLException e) {
+				logger.warning("MalformedURLException encountered: " + e.getMessage());
+				return Status.error("MalformedURLException during creation of diagram html URL");
+			}
+		} catch (JavaModelException e) {
+			logger.warning("JavaModelException encountered: " + e.getMessage());
+			return Status.error("JavaModelException during creation of output folders");
+		} catch (JsonProcessingException e) {
+			logger.warning("JsonProcessingException encountered: " + e.getMessage());
+			return Status.error("JsonProcessingException during serialization of diagram relations");
+		} catch (IOException e) {
+			logger.warning("IOException encountered: " + e.getMessage());
+			return Status.error("IOException during writing json to the output file");
+		}
+
+		var end = Instant.now();
+
+		logger.info("JSON " + estimation + " export report job for " + iJavaProject.getElementName() + " took: "
+				+ Duration.between(start, end).getSeconds() + " seconds");
+
+		return Status.OK_STATUS;
+	}
+
+	public boolean belongsTo(Object family) {
+		return FAMILY.equals(family);
+	}
+}
